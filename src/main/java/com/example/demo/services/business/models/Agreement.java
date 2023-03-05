0b3c1c25cd8275d8caaaa2f811abcecfb5ef368b
Jarl André Hübenthal <jarlah@protonmail.com>
2023-03-05 16:42:14 +0100

    doc

--- src/main/java/com/example/demo/services/business/models/Agreement.java
+++ src/main/java/com/example/demo/services/business/models/Agreement.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 public record Agreement(
+    // Serves as the "avtalenummer"
     UUID id,
     AgreementStatus status,
     BigDecimal agreementPrice,
