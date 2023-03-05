7190ec976a16ccf5594488521edec8a027a7b443
Jarl André Hübenthal <jarlah@protonmail.com>
2023-03-05 09:38:23 +0100

    format

--- src/main/java/com/example/demo/web/filter/CorrelationIdFilter.java
+++ src/main/java/com/example/demo/web/filter/CorrelationIdFilter.java
@@ -5,26 +5,26 @@ import jakarta.ws.rs.container.ContainerRequestFilter;
 import jakarta.ws.rs.container.ContainerResponseContext;
 import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.ext.Provider;
-import org.slf4j.MDC;
-
 import java.util.UUID;
+import org.slf4j.MDC;
 
 @Provider
 public class CorrelationIdFilter implements ContainerRequestFilter, ContainerResponseFilter {
-    private static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-Id";
-    private static final String MDC_CORRELATION_ID_KEY = "correlationId";
+  private static final String CORRELATION_ID_HEADER_NAME = "X-Correlation-Id";
+  private static final String MDC_CORRELATION_ID_KEY = "correlationId";
 
-    @Override
-    public void filter(ContainerRequestContext requestContext) {
-        String correlationId = requestContext.getHeaderString(CORRELATION_ID_HEADER_NAME);
-        if (correlationId == null || correlationId.isEmpty()) {
-            correlationId = UUID.randomUUID().toString();
-        }
-        MDC.put(MDC_CORRELATION_ID_KEY, correlationId);
+  @Override
+  public void filter(ContainerRequestContext requestContext) {
+    String correlationId = requestContext.getHeaderString(CORRELATION_ID_HEADER_NAME);
+    if (correlationId == null || correlationId.isEmpty()) {
+      correlationId = UUID.randomUUID().toString();
     }
+    MDC.put(MDC_CORRELATION_ID_KEY, correlationId);
+  }
 
-    @Override
-    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
-        MDC.remove(MDC_CORRELATION_ID_KEY);
-    }
+  @Override
+  public void filter(
+      ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+    MDC.remove(MDC_CORRELATION_ID_KEY);
+  }
 }
